096921f5b2c53c2c30d01662db5a7d3721967e0d
Jonathan Feitosa <jonathan.souza@itriad.org.br>
2020-11-11 17:02:38 -0400

    Adding search system

--- app/src/main/java/br/com/listgistgithub/ui/adapter/HomeAdapter.kt
+++ app/src/main/java/br/com/listgistgithub/ui/adapter/HomeAdapter.kt
@@ -6,19 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import br.com.listgistgithub.R
 import br.com.listgistgithub.databinding.ItemGistBinding
 import br.com.listgistgithub.model.Gist
+import java.util.*
+import kotlin.collections.ArrayList
 
 class HomeAdapter(
     private val context: Context,
     private val list: ArrayList<Gist>,
     private val onItemClickListener: ((gist: Gist) -> Unit)
-) : RecyclerView.Adapter<HomeAdapter.DataViewHolder>() {
+) : RecyclerView.Adapter<HomeAdapter.DataViewHolder>(), Filterable {
 
     private var lastPosition = -1
     lateinit var favoriteListener: OnFavoriteClickListener
+    lateinit var listGistFilter: List<Gist>
+    lateinit var listGistFull: ArrayList<Gist>
     lateinit var unFavoriteListener: OnUnFavoriteClickListener
 
     inner class DataViewHolder(
@@ -66,10 +72,41 @@ class HomeAdapter(
         setAnimation(holder.itemView, position)
     }
 
-    fun addGist(gists: List<Gist>) {
-        this.list.apply {
-            addAll(gists)
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charSearch = constraint.toString()
+                listGistFilter = if (charSearch.isEmpty()) {
+                    listGistFull
+                } else {
+                    val resultList = listGistFull.filter {
+                        (it.owner.login).toLowerCase(Locale.ROOT)
+                            .contains(charSearch.toLowerCase(Locale.ROOT))
+                    }
+                    resultList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = listGistFilter
+                return filterResults
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                results?.let {
+                    it.values?.let {
+                        listGistFilter = results.values as ArrayList<Gist>
+                        clearItems()
+                        list.addAll(listGistFilter)
+                        notifyDataSetChanged()
+                    }
+                }
+            }
         }
+    }
+
+    fun addGist(gists: List<Gist>) {
+        list.addAll(gists)
+        listGistFull = ArrayList(list)
     }
 
     fun clearItems() = list.clear()
--- app/src/main/java/br/com/listgistgithub/ui/home/view/HomeFragment.kt
+++ app/src/main/java/br/com/listgistgithub/ui/home/view/HomeFragment.kt
@@ -1,9 +1,10 @@
 package br.com.listgistgithub.ui.home.view
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -19,16 +20,17 @@ import br.com.listgistgithub.utils.NetworkResponse
 import kotlinx.android.synthetic.main.fragment_home.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
+class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener,
+    SearchView.OnQueryTextListener {
 
     private val viewModel by viewModel<HomeViewModel>()
-
 
     private lateinit var adapter: HomeAdapter
     private var pastVisiblesItems = 0
     private var totalItemCount = 0
     private var loading = false
     private var pageLoad = 0
+    private var searching = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -41,6 +43,21 @@ class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
         viewModel.getFavorites(requireActivity())
         setupObservers()
         swiperefresh.setOnRefreshListener(this)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.search_menu, menu)
+
+        val searchManager =
+            requireContext().getSystemService(Context.SEARCH_SERVICE) as SearchManager?
+        val searchMenuItem = menu.findItem(R.id.search)
+        val searchView = searchMenuItem.actionView as SearchView
+        searchView.setSearchableInfo(searchManager!!.getSearchableInfo(requireActivity().componentName))
+        searchView.isSubmitButtonEnabled = true
+        searchView.setOnQueryTextListener(this)
     }
 
     private fun setupRecyclerView() {
@@ -48,7 +65,7 @@ class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
         recyclerViewGists.setHasFixedSize(true)
         recyclerViewGists.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                if (dy > 0) addingMoreGists()
+                if (dy > 0 && !searching) addingMoreGists()
             }
         })
     }
@@ -94,11 +111,6 @@ class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
         })
     }
 
-    override fun onRefresh() {
-        viewModel.getFavorites(requireActivity())
-        setupObservers()
-    }
-
     private fun addingMoreGists() {
         totalItemCount = recyclerViewGists.layoutManager!!.itemCount
         pastVisiblesItems =
@@ -120,5 +132,24 @@ class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
             addGist(gists)
             notifyDataSetChanged()
         }
+    }
+
+    override fun onRefresh() {
+        adapter.clearItems()
+        viewModel.getFavorites(requireActivity())
+        setupObservers()
+    }
+
+    override fun onQueryTextSubmit(p0: String?): Boolean {
+        return false
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+
+        p0?.let {
+            searching = p0.isNotEmpty()
+        }
+        adapter.filter.filter(p0)
+        return false
     }
 }
